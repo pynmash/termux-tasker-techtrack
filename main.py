5df6528a8aaa8108b0fc42c96a9b29ba20690406
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-01-22 12:25:45 +0000

    Add logic for determining if phrase exists

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.civil-service-careers.gov.uk/professions/working-in-digital-data-and-technology/techtrack"
+phrase = "Register here to find out when applications open for the second cohort of Software Developer in early 2025."
+
+
+def phrase_exists(links, phrase):
+    for link in links:
+        if phrase in link:
+            return True
+    return False
+
+
+response = requests.get(url=URL)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+
+links = [link for link in soup.find_all("a")]
+
+print(phrase_exists(links, phrase))
